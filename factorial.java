400719f38bc8e21c06e2cb9db046ea6f6b9ddb38
HAPPYS1NGH <harpreetsingh140503@gmail.com>
2023-01-31 18:59:37 +0530

    first commit

---
+++ factorial.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class factorial {
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int a = sc.nextInt();
+        System.out.println("Number is " + a);
+    }
+}
