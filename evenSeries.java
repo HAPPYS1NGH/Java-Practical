9326b712d74edf043c7a94b5cf5905e997e35f67
HAPPYS1NGH <harpreetsingh140503@gmail.com>
2023-02-05 20:51:53 +0530

    Added Q5

---
+++ evenSeries.java
@@ -0,0 +1,9 @@
+
+public class evenSeries {
+    public static void main(String args[]) {
+        System.out.println("The series is :");
+        for (int i = 50; i >= 0; i-=2) {
+            System.out.println(i+ "\t");
+        }
+    }
+}
